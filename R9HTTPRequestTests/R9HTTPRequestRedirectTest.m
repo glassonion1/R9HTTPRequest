bb2abdf54a124725b07dfd912f5c9fb021cf0df1
Taisuke Fujita <glassonion999@gmail.com>
2012-07-02 01:28:19 +0900

    Add test of redirect and WSSE.

---
+++ R9HTTPRequestTests/R9HTTPRequestRedirectTest.m
@@ -0,0 +1,13 @@
+//
+//  R9HTTPRequestRedirectTest.m
+//  R9HTTPRequest
+//
+//  Created by taisuke fujita on 12/07/02.
+//  Copyright (c) 2012年 Revolution 9. All rights reserved.
+//
+
+#import "R9HTTPRequestRedirectTest.h"
+
+@implementation R9HTTPRequestRedirectTest
+
+@end
